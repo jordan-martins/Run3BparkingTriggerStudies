5e1a47d0e7915eb8b1349d0954b38f6a02d77f15
Jordan Martins <36475694+jordan-martins@users.noreply.github.com>
2022-01-18 10:12:58 +0100

    Update denom.C
    adding a ";" after nbins variable definition

--- eff/denom.C
+++ eff/denom.C
@@ -2,7 +2,7 @@
   TFile f("root/genstudy_pt.root");
   TTree* t = (TTree*)f.Get("tree");
   std::cout << "entries: " << t->GetEntries() << std::endl;
-  nbins = 13
+  nbins = 13;
   TH2F denom("denom","denom",nbins, 0. ,nbins*1., nbins, 0. ,nbins*1. );
   int n = tree->Draw("gen_e1_pt:gen_e2_pt>>denom","","goff");
   std::cout << "processed: " << n << std::endl;
